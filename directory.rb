7add51004c8ceeed5b867030e2deb692e21e4a19
Ben Dowsett <bendowsett@gmail.com>
2021-10-13 12:32:44 +0000

    interactive menu

--- directory.rb
+++ directory.rb
@@ -14,6 +14,21 @@ students = [
 {name: "Norman Bates", cohort: :november}
 ]
 
+def input_students
+    puts "Please enter the names of the students"
+    puts "To finish, just hit return twice"
+    
+    students = []
+    
+    name = gets.chomp
+    
+    while !name.empty? do
+        students << {name: name, cohort: :november}
+        puts "Now we have #{students.count} students"
+        name = gets.chomp
+    end 
+    students
+end
 
 def print_header 
   puts "The students of Villians Academy"
@@ -21,17 +36,44 @@ def print_header
 end 
 
 def print(students)
-students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+students.each_with_index do |student, idx|
+    puts "#{idx + 1}: #{student[:name]} (#{student[:cohort]} cohort)"
 end 
 end
 
+def interactive_menu
+    students = []
+    loop do
+        puts "1. Input the students"
+        puts "2. Show the students"
+        puts "9. Exit"
+        selection = gets.chomp
+        
+        case selection
+        when "1"
+            students = input_students
+        when "2"
+            print_header
+            print(students)
+            print_footer(students)
+        when "9"
+            exit
+        else 
+            puts "I dont know what you meant, try again"
+        end 
+        
+    end 
+end 
 
 def print_footer(students)
 puts "Overall, we have #{students.count} great students"
 end 
 
-
+=begin
+students = input_students
 print_header
 print(students)
 print_footer(students)
+=end 
+
+interactive_menu
